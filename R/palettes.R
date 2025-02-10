ca431316cc2bc10c2541eedb710dc2028279da2f
claudia-laurenzano <claudialaurenzano@gmail.com>
2025-02-10 15:24:22 -0600

    Initial commit

--- R/palettes.R
+++ R/palettes.R
@@ -1,4 +1,4 @@
-#' Sunset palettes
+#' Blanco palettes
 #'
 #' Palettes inspired by sunsets in the Canadian Prairies.
 #'
@@ -7,7 +7,7 @@
 #' color palettes. Use `names(blanco_palettes)` to return all palette names.
 #' @source <https://github.com/claudia-laurenzano/blancocentR>
 #' @author [Claudia Laurenzano](https://github.com/claudia-laurenzano)
-#' @seealso [pal_palette()], [pal_colour()]
+#' @seealso [pal_palette()], [pal_color()]
 #' @examples
 #' # Get all palettes by name.
 #' names(blanco_palettes)
@@ -17,17 +17,31 @@
 "blanco_palettes"
 
 #' @rdname blanco_palettes
+#' @format ## `custom`
+#' An object of class `palettes_palette` with
+#' `r length(custom)` sequential colour palettes.
+#'  Use `names(custom)` to return all palette names.
+"custom"
+
+#' @rdname blanco_palettes
+#' @format ## `gradients`
+#' An object of class `palettes_palette` with
+#' `r length(gradients)` discrete color palettes.
+#'  Use `names(gradients)` to return all palette names.
+"gradients"
+
+#' @rdname blanco_palettes
 #' @format ## `grays`
 #' An object of class `palettes_palette` with
 #' `r length(grays)` discrete color palettes.
 #'  Use `names(grays)` to return all palette names.
 "grays"
 
-#' @rdname sunset_palettes
-#' @format ## `custom`
+#' @rdname blanco_palettes
+#' @format ## `misc_pal`
 #' An object of class `palettes_palette` with
-#' `r length(custom)` sequential colour palettes.
-#'  Use `names(custom)` to return all palette names.
-"custom"
+#' `r length(misc_pal)` sequential colour palettes.
+#'  Use `names(misc_pal)` to return all palette names.
+"misc_pal"
 
 # document()
