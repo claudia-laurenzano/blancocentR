ad85157bf8ede4baef2622090cd1ead9da1bcb8b
claudia-laurenzano <claudialaurenzano@gmail.com>
2025-02-12 13:03:00 -0600

    Initial commit

--- R/palettes.R
+++ R/palettes.R
@@ -43,3 +43,130 @@
 #' `r length(misc_pal)` discrete colors.
 #'  Use `names(misc_pal)` to return all color names.
 "misc_pal"
+
+#' @rdname blanco_palettes
+#' @format ## `cols_dcfs`
+#' An object of class `palettes_palette` with
+#' `r length(cols_dcfs)` discrete colors.
+#'  Use `names(cols_dcfs)` to return all color names.
+"cols_dcfs"
+
+#' @rdname blanco_palettes
+#' @format ## `cols_dcfs1`
+#' An object of class `palettes_palette` with
+#' `r length(cols_dcfs1)` discrete colors.
+#'  Use `names(cols_dcfs1)` to return all color names.
+"cols_dcfs1"
+
+#' @rdname blanco_palettes
+#' @format ## `cols_dcfs1_nm`
+#' An object of class `palettes_palette` with
+#' `r length(cols_dcfs1_nm)` discrete colors.
+#'  Use `names(cols_dcfs1_nm)` to return all color names.
+"cols_dcfs1_nm"
+
+#' @rdname blanco_palettes
+#' @format ## `cols_dcfs2`
+#' An object of class `palettes_palette` with
+#' `r length(cols_dcfs2)` discrete colors.
+#'  Use `names(cols_dcfs2)` to return all color names.
+"cols_dcfs2"
+
+#' @rdname blanco_palettes
+#' @format ## `cols_dcfs2_nm`
+#' An object of class `palettes_palette` with
+#' `r length(cols_dcfs2_nm)` discrete colors.
+#'  Use `names(cols_dcfs2_nm)` to return all color names.
+"cols_dcfs2_nm"
+
+#' @rdname blanco_palettes
+#' @format ## `cols_dcfs_nm`
+#' An object of class `palettes_palette` with
+#' `r length(cols_dcfs_nm)` discrete colors.
+#'  Use `names(cols_dcfs_nm)` to return all color names.
+"cols_dcfs_nm"
+
+#' @rdname blanco_palettes
+#' @format ## `cols_de`
+#' An object of class `palettes_palette` with
+#' `r length(cols_de)` discrete colors.
+#'  Use `names(cols_de)` to return all color names.
+"cols_de"
+
+#' @rdname blanco_palettes
+#' @format ## `cols_de1`
+#' An object of class `palettes_palette` with
+#' `r length(cols_de1)` discrete colors.
+#'  Use `names(cols_de1)` to return all color names.
+"cols_de1"
+
+#' @rdname blanco_palettes
+#' @format ## `cols_de1_nm`
+#' An object of class `palettes_palette` with
+#' `r length(cols_de1_nm)` discrete colors.
+#'  Use `names(cols_de1_nm)` to return all color names.
+"cols_de1_nm"
+
+#' @rdname blanco_palettes
+#' @format ## `cols_de2`
+#' An object of class `palettes_palette` with
+#' `r length(cols_de2)` discrete colors.
+#'  Use `names(cols_de2)` to return all color names.
+"cols_de2"
+
+#' @rdname blanco_palettes
+#' @format ## `cols_de2_nm`
+#' An object of class `palettes_palette` with
+#' `r length(cols_de2_nm)` discrete colors.
+#'  Use `names(cols_de2_nm)` to return all color names.
+"cols_de2_nm"
+
+#' @rdname blanco_palettes
+#' @format ## `cols_de_nm`
+#' An object of class `palettes_palette` with
+#' `r length(cols_de_nm)` discrete colors.
+#'  Use `names(cols_de_nm)` to return all color names.
+"cols_de_nm"
+
+#' @rdname blanco_palettes
+#' @format ## `cols_ldh`
+#' An object of class `palettes_palette` with
+#' `r length(cols_ldh)` discrete colors.
+#'  Use `names(cols_ldh)` to return all color names.
+"cols_ldh"
+
+#' @rdname blanco_palettes
+#' @format ## `cols_ldh1`
+#' An object of class `palettes_palette` with
+#' `r length(cols_ldh1)` discrete colors.
+#'  Use `names(cols_ldh1)` to return all color names.
+"cols_ldh1"
+
+#' @rdname blanco_palettes
+#' @format ## `cols_ldh1_nm`
+#' An object of class `palettes_palette` with
+#' `r length(cols_ldh1_nm)` discrete colors.
+#'  Use `names(cols_ldh1_nm)` to return all color names.
+"cols_ldh1_nm"
+
+#' @rdname blanco_palettes
+#' @format ## `cols_ldh2`
+#' An object of class `palettes_palette` with
+#' `r length(cols_ldh2)` discrete colors.
+#'  Use `names(cols_ldh2)` to return all color names.
+"cols_ldh2"
+
+#' @rdname blanco_palettes
+#' @format ## `cols_ldh2_nm`
+#' An object of class `palettes_palette` with
+#' `r length(cols_ldh2_nm)` discrete colors.
+#'  Use `names(cols_ldh2_nm)` to return all color names.
+"cols_ldh2_nm"
+
+#' @rdname blanco_palettes
+#' @format ## `cols_ldh_nm`
+#' An object of class `palettes_palette` with
+#' `r length(cols_ldh_nm)` discrete colors.
+#'  Use `names(cols_ldh_nm)` to return all color names.
+"cols_ldh_nm"
+
--- data-raw/colors_bc.R
+++ data-raw/colors_bc.R
@@ -45,149 +45,149 @@ usethis::use_data(gradients, overwrite = TRUE)
 
 
 
-# # Regions DE --------------------------------------------------------------
-#
-# # Planning and Development (Digital Equity)
-#
-# # large area regions
-# list_de <- list(
-#   "1" = "#27415d",
-#   "2" = "#556765",
-#   "3" = "#b2a28b",
-#   "4" = "#ac5341",
-#   "5" = "#d2a95b",
-#   "6" = "#62a3e9",
-#   "7" = "#407f8f",
-#   "8" = "#c9f3ef"
-# )
-#
-# cols_de <- as_palette(list_de)
-# usethis::use_data(cols_de, overwrite = TRUE)
-#
-# list_de_nm <- list(
-#   "Orleans" = "#27415d",
-#   "Capital Region" = "#556765",
-#   "South Central" = "#b2a28b",
-#   "Acadiana" = "#ac5341",
-#   "Southwest" = "#d2a95b",
-#   "Kisatchie-Delta" = "#62a3e9",
-#   "Coordinating and Development District" = "#407f8f",
-#   "North Delta" = "#c9f3ef")
-#
-# cols_de_nm <- as_palette(list_de_nm)
-# usethis::use_data(cols_de_nm, overwrite = TRUE)
-#
-# # small area regions (1)
-# cols_de1 <- as_palette(replace(list_de, list_de == "#c9f3ef", "#B8DAD7"))
-# cols_de1_nm = as_palette(replace(list_de_nm, list_de_nm == "#c9f3ef", "#B8DAD7"))
-#
-# usethis::use_data(cols_de1, overwrite = TRUE)
-# usethis::use_data(cols_de1_nm, overwrite = TRUE)
-#
-# # small area regions (2)
-# cols_de2 <- as_palette(replace(list_de, list_de == "#c9f3ef", "#9FCCC8"))
-# cols_de2_nm = as_palette(replace(list_de_nm, list_de_nm == "#c9f3ef", "#9FCCC8"))
-#
-# usethis::use_data(cols_de2, overwrite = TRUE)
-# usethis::use_data(cols_de2_nm, overwrite = TRUE)
-#
-#
-#
-# # Regions DCFS ------------------------------------------------------------
-#
-# # DCFS regions
-#
-# list_dcfs = list("1" = "#27415d",
-#                  "2" = "#556765",
-#                  "3" = "#77839b",
-#                  "4" = "#b2a28b",
-#                  "5" = "#ac5341",
-#                  "6" = "#d2a95b",
-#                  "7" = "#62a3e9",
-#                  "8" = "#407f8f",
-#                  "9" = "#c9f3ef")
-#
-# cols_dcfs <- as_palette(list_dcfs)
-# usethis::use_data(cols_dcfs, overwrite = TRUE)
-#
-# list_dcfs_nm = list("Orleans"      = "#27415d",
-#                     "Baton Rouge"  = "#556765",
-#                     "Covington"    = "#77839b",
-#                     "Thibodaux"    = "#b2a28b",
-#                     "Lafayette"    = "#ac5341",
-#                     "Lake Charles" = "#d2a95b",
-#                     "Alexandria"   = "#62a3e9",
-#                     "Shreveport"   = "#407f8f",
-#                     "Monroe"   = "#c9f3ef")
-#
-# cols_dcfs_nm <- as_palette(list_dcfs_nm)
-# usethis::use_data(cols_dcfs_nm, overwrite = TRUE)
-#
-# # small area regions (1)
-# cols_dcfs1 <- as_palette(replace(list_dcfs, list_dcfs == "#c9f3ef", "#B8DAD7"))
-# cols_dcfs1_nm = as_palette(replace(list_dcfs_nm, list_dcfs_nm == "#c9f3ef", "#B8DAD7"))
-#
-# usethis::use_data(cols_dcfs1, overwrite = TRUE)
-# usethis::use_data(cols_dcfs1_nm, overwrite = TRUE)
-#
-#
-# # small area regions (2)
-# cols_dcfs2 <- as_palette(replace(list_dcfs, list_dcfs == "#c9f3ef", "#9FCCC8"))
-# cols_dcfs2_nm = as_palette(replace(list_dcfs_nm, list_dcfs_nm == "#c9f3ef", "#9FCCC8"))
-#
-# usethis::use_data(cols_dcfs2, overwrite = TRUE)
-# usethis::use_data(cols_dcfs2_nm, overwrite = TRUE)
-#
-#
-#
-# # Regions LDH -------------------------------------------------------------
-#
-# # LDH regions
-#
-# list_ldh = list(
-#   "1" = "#27415d",
-#   "2" = "#556765",
-#   "3" = "#b2a28b",
-#   "5" = "#d2a95b",
-#   "4" = "#ac5341",
-#   "6" = "#62a3e9",
-#   "7" = "#407f8f",
-#   "8" = "#c9f3ef",
-#   "9" = "#77839b"
-# )
-#
-# cols_ldh <- as_palette(list_ldh)
-# usethis::use_data(cols_ldh, overwrite = TRUE)
-#
-# list_ldh_nm = list(
-#   "Greater New Orleans Area"  = "#27415d",
-#   "Capital Area"              = "#556765",
-#   "South Central Louisiana"   = "#b2a28b",
-#   "Southwest Louisiana"       = "#d2a95b",
-#   "Acadiana"                  = "#ac5341",
-#   "Central Louisiana"         = "#62a3e9",
-#   "Northwest Louisiana"       = "#407f8f",
-#   "Northeast Louisiana"       = "#c9f3ef",
-#   "Northshore Area"           = "#77839b"
-# )
-#
-# cols_ldh_nm <- as_palette(list_ldh_nm)
-# usethis::use_data(cols_ldh_nm, overwrite = TRUE)
-#
-# # small area regions (1)
-# cols_ldh1 <- as_palette(replace(list_ldh, list_ldh == "#c9f3ef", "#B8DAD7"))
-# cols_ldh1_nm = as_palette(replace(list_ldh_nm, list_ldh_nm == "#c9f3ef", "#B8DAD7"))
-#
-# usethis::use_data(cols_ldh1, overwrite = TRUE)
-# usethis::use_data(cols_ldh1_nm, overwrite = TRUE)
-#
-#
-# # small area regions (2)
-# cols_ldh2 <- as_palette(replace(list_ldh, list_ldh == "#c9f3ef", "#9FCCC8"))
-# cols_ldh2_nm = as_palette(replace(list_ldh_nm, list_ldh_nm == "#c9f3ef", "#9FCCC8"))
-#
-# usethis::use_data(cols_ldh2, overwrite = TRUE)
-# usethis::use_data(cols_ldh2_nm, overwrite = TRUE)
+# Regions DE --------------------------------------------------------------
+
+# Planning and Development (Digital Equity)
+
+# large area regions
+list_de <- list(
+  "1" = "#27415d",
+  "2" = "#556765",
+  "3" = "#b2a28b",
+  "4" = "#ac5341",
+  "5" = "#d2a95b",
+  "6" = "#62a3e9",
+  "7" = "#407f8f",
+  "8" = "#c9f3ef"
+)
+
+cols_de <- as_palette(list_de)
+usethis::use_data(cols_de, overwrite = TRUE)
+
+list_de_nm <- list(
+  "Orleans" = "#27415d",
+  "Capital Region" = "#556765",
+  "South Central" = "#b2a28b",
+  "Acadiana" = "#ac5341",
+  "Southwest" = "#d2a95b",
+  "Kisatchie-Delta" = "#62a3e9",
+  "Coordinating and Development District" = "#407f8f",
+  "North Delta" = "#c9f3ef")
+
+cols_de_nm <- as_palette(list_de_nm)
+usethis::use_data(cols_de_nm, overwrite = TRUE)
+
+# small area regions (1)
+cols_de1 <- as_palette(replace(list_de, list_de == "#c9f3ef", "#B8DAD7"))
+cols_de1_nm = as_palette(replace(list_de_nm, list_de_nm == "#c9f3ef", "#B8DAD7"))
+
+usethis::use_data(cols_de1, overwrite = TRUE)
+usethis::use_data(cols_de1_nm, overwrite = TRUE)
+
+# small area regions (2)
+cols_de2 <- as_palette(replace(list_de, list_de == "#c9f3ef", "#9FCCC8"))
+cols_de2_nm = as_palette(replace(list_de_nm, list_de_nm == "#c9f3ef", "#9FCCC8"))
+
+usethis::use_data(cols_de2, overwrite = TRUE)
+usethis::use_data(cols_de2_nm, overwrite = TRUE)
+
+
+
+# Regions DCFS ------------------------------------------------------------
+
+# DCFS regions
+
+list_dcfs = list("1" = "#27415d",
+                 "2" = "#556765",
+                 "3" = "#77839b",
+                 "4" = "#b2a28b",
+                 "5" = "#ac5341",
+                 "6" = "#d2a95b",
+                 "7" = "#62a3e9",
+                 "8" = "#407f8f",
+                 "9" = "#c9f3ef")
+
+cols_dcfs <- as_palette(list_dcfs)
+usethis::use_data(cols_dcfs, overwrite = TRUE)
+
+list_dcfs_nm = list("Orleans"      = "#27415d",
+                    "Baton Rouge"  = "#556765",
+                    "Covington"    = "#77839b",
+                    "Thibodaux"    = "#b2a28b",
+                    "Lafayette"    = "#ac5341",
+                    "Lake Charles" = "#d2a95b",
+                    "Alexandria"   = "#62a3e9",
+                    "Shreveport"   = "#407f8f",
+                    "Monroe"   = "#c9f3ef")
+
+cols_dcfs_nm <- as_palette(list_dcfs_nm)
+usethis::use_data(cols_dcfs_nm, overwrite = TRUE)
+
+# small area regions (1)
+cols_dcfs1 <- as_palette(replace(list_dcfs, list_dcfs == "#c9f3ef", "#B8DAD7"))
+cols_dcfs1_nm = as_palette(replace(list_dcfs_nm, list_dcfs_nm == "#c9f3ef", "#B8DAD7"))
+
+usethis::use_data(cols_dcfs1, overwrite = TRUE)
+usethis::use_data(cols_dcfs1_nm, overwrite = TRUE)
+
+
+# small area regions (2)
+cols_dcfs2 <- as_palette(replace(list_dcfs, list_dcfs == "#c9f3ef", "#9FCCC8"))
+cols_dcfs2_nm = as_palette(replace(list_dcfs_nm, list_dcfs_nm == "#c9f3ef", "#9FCCC8"))
+
+usethis::use_data(cols_dcfs2, overwrite = TRUE)
+usethis::use_data(cols_dcfs2_nm, overwrite = TRUE)
+
+
+
+# Regions LDH -------------------------------------------------------------
+
+# LDH regions
+
+list_ldh = list(
+  "1" = "#27415d",
+  "2" = "#556765",
+  "3" = "#b2a28b",
+  "5" = "#d2a95b",
+  "4" = "#ac5341",
+  "6" = "#62a3e9",
+  "7" = "#407f8f",
+  "8" = "#c9f3ef",
+  "9" = "#77839b"
+)
+
+cols_ldh <- as_palette(list_ldh)
+usethis::use_data(cols_ldh, overwrite = TRUE)
+
+list_ldh_nm = list(
+  "Greater New Orleans Area"  = "#27415d",
+  "Capital Area"              = "#556765",
+  "South Central Louisiana"   = "#b2a28b",
+  "Southwest Louisiana"       = "#d2a95b",
+  "Acadiana"                  = "#ac5341",
+  "Central Louisiana"         = "#62a3e9",
+  "Northwest Louisiana"       = "#407f8f",
+  "Northeast Louisiana"       = "#c9f3ef",
+  "Northshore Area"           = "#77839b"
+)
+
+cols_ldh_nm <- as_palette(list_ldh_nm)
+usethis::use_data(cols_ldh_nm, overwrite = TRUE)
+
+# small area regions (1)
+cols_ldh1 <- as_palette(replace(list_ldh, list_ldh == "#c9f3ef", "#B8DAD7"))
+cols_ldh1_nm = as_palette(replace(list_ldh_nm, list_ldh_nm == "#c9f3ef", "#B8DAD7"))
+
+usethis::use_data(cols_ldh1, overwrite = TRUE)
+usethis::use_data(cols_ldh1_nm, overwrite = TRUE)
+
+
+# small area regions (2)
+cols_ldh2 <- as_palette(replace(list_ldh, list_ldh == "#c9f3ef", "#9FCCC8"))
+cols_ldh2_nm = as_palette(replace(list_ldh_nm, list_ldh_nm == "#c9f3ef", "#9FCCC8"))
+
+usethis::use_data(cols_ldh2, overwrite = TRUE)
+usethis::use_data(cols_ldh2_nm, overwrite = TRUE)
 
 
 
@@ -231,26 +231,26 @@ blanco_palettes <- c(
   custom,
   gradients,
 
-  # cols_de,
-  # cols_de1,
-  # cols_de2,
-  # cols_de_nm,
-  # cols_de1_nm,
-  # cols_de2_nm,
-  #
-  # cols_dcfs,
-  # cols_dcfs1,
-  # cols_dcfs2,
-  # cols_dcfs_nm,
-  # cols_dcfs1_nm,
-  # cols_dcfs2_nm,
-  #
-  # cols_ldh,
-  # cols_ldh1,
-  # cols_ldh2,
-  # cols_ldh_nm,
-  # cols_ldh1_nm,
-  # cols_ldh2_nm,
+  cols_de,
+  cols_de1,
+  cols_de2,
+  cols_de_nm,
+  cols_de1_nm,
+  cols_de2_nm,
+
+  cols_dcfs,
+  cols_dcfs1,
+  cols_dcfs2,
+  cols_dcfs_nm,
+  cols_dcfs1_nm,
+  cols_dcfs2_nm,
+
+  cols_ldh,
+  cols_ldh1,
+  cols_ldh2,
+  cols_ldh_nm,
+  cols_ldh1_nm,
+  cols_ldh2_nm,
 
   misc_pal
 )
