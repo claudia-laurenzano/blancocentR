35986c8abda2c92f728e70838061d0acb51f6bbc
claudia-laurenzano <claudialaurenzano@gmail.com>
2025-02-19 16:41:19 -0600

    Initial commit

--- R/blancocentR-package.R
+++ R/blancocentR-package.R
@@ -6,4 +6,14 @@
 #' @keywords internal
 #' @name blancocentR
 #' @export dark_text
+#' @export light_text
+#' @export mid_gray
+#' @export light_gray
+#' @export ull_red
+#' @export col_la
+#' @export col_us
+#' @export col_ur
+#' @export col_ru
+#' @export misc_pal
+
 NULL
